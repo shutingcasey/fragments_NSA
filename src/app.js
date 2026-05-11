8fcdcbd697b5406405b8e3405091da297894b9e9
shutingcasey <shutingcasey@gmail.com>
2026-05-11 10:04:39 -0400

    Initial work on express app and server

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
     description: 'fragments service running normally',
     author,
     // TODO: change this to use your GitHub username!
-    githubUrl: 'https://github.com/REPLACE_WITH_YOUR_GITHUB_USERNAME/fragments',
+    githubUrl: 'https://github.com/shutingcasey/fragments',
     version,
     timestamp: new Date().toISOString(),
   });
