8cbde2daa2553f923209d77159054089ac3d14e2
shutingcasey <shutingcasey@gmail.com>
2026-05-25 20:47:50 -0400

    Finished Configure Unit Tests to Use Basic Auth

--- src/auth/cognito.js
+++ src/auth/cognito.js
@@ -1,4 +1,4 @@
-// src/auth.js
+// src/auth/cognito.js
 
 // Configure a JWT token strategy for Passport based on
 // Identity Token provided by Cognito. The token will be
@@ -8,7 +8,7 @@ const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
 
-const logger = require('./logger');
+const logger = require('../logger');
 
 // We expect AWS_COGNITO_POOL_ID and AWS_COGNITO_CLIENT_ID to be defined.
 if (!(process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID)) {
