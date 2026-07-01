6aeebab100d1f802005a945a7764bf24dc7ad0ae
shutingcasey <shutingcasey@gmail.com>
2026-07-01 16:25:06 -0400

    Finished As 1

--- src/app.js
+++ src/app.js
@@ -28,7 +28,9 @@ app.use(pino);
 app.use(helmet());
 
 // Use CORS middleware so we can make requests across origins
-app.use(cors());
+app.use(cors({
+  exposedHeaders: ['Location'],
+}));
 
 // Use gzip/deflate compression middleware
 app.use(compression());
